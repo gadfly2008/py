82004cf34e102c76750c2a7ab4e103527b01875d
gadfly2008 <gadfly2008@gmail.com>
2014-01-14 10:49:19 +0800

    delete

--- logmonitor/eagleeye/rcms/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('rcms.views',
-    (r'nla/devices/', 'search_nla_devices'),
-    (r'fc/devices/', 'search_fc_devices'),
-    (r'customers/', 'search_customers'),
-    (r'channels/', 'search_channels'),
-)
--- logmonitor/eagleeye/startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/python manage.py runfcgi method=prefork socket=/tmp/eagleeye.sock pidfile=/tmp/eagleeye.pid maxrequests=128 maxchildren=8
